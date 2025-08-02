9541cd9f11d7c7ec096b932c5b0ce69919d02727
Irindominic <162862207+Irindominic@users.noreply.github.com>
2025-08-02 07:31:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os  # For running GUI script in a new command window (Windows)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start')
+def start_script():
+    # Windows-specific: opens handsome.py in a new terminal window
+    os.system('start python handsound.py')
+    return 'Swara Player Started! You can close this tab now.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
